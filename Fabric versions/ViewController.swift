17d12b4c75e84ee6f4837c44bd839c1f04bf939f
BoogieMonster1O1 <harrypotter.starwars.shrish@gmail.com>
2020-06-12 10:41:02 +0530

    Disabled zooming

--- Fabric versions/ViewController.swift
+++ Fabric versions/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.configuration.userContentController.addUserScript(self.getZoomDisableScript())
         if let url = URL(string: "https://boogiemonster1o1.github.io/a.html") {
             let request = URLRequest(url: url)
             webView.load(request)
@@ -21,6 +22,14 @@ class ViewController: UIViewController {
     }
     override func loadView() {
         self.view = webView
+    }
+    
+    private func getZoomDisableScript() -> WKUserScript {
+        let source: String = "var meta = document.createElement('meta');" +
+            "meta.name = 'viewport';" +
+            "meta.content = 'width=device-width, initial-scale=1.0, maximum- scale=1.0, user-scalable=no';" +
+            "var head = document.getElementsByTagName('head')[0];" + "head.appendChild(meta);"
+        return WKUserScript(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
     }
 
 }
